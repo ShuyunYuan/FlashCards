1a16d7a166d0d9e692e840dc2b0f9c44a3457b43
ZhuHanJiang <zhhjiang@ucdavis.edu>
2019-05-17 00:13:28 -0700

    combined server file

--- createDB.js
+++ createDB.js
@@ -9,8 +9,16 @@ const db = new sqlite3.Database(dbFileName);  // object, not database.
 // Initialize table.
 // If the table already exists, causes an error.
 // Fix the error by removing or renaming Flashcards.db
-const cmdStr = 'CREATE TABLE Flashcards (user INT )'
-db.run(cmdStr,tableCreationCallback);
+const cmdStr = 'CREATE TABLE Flashcards (user INT, english TEXT, chinese TEXT, seen INT, correct INT)';
+//db.run(cmdStr,tableCreationCallback);
+
+const eng = "hello";
+const chi = "你好";
+const cmdStr_in = 'INSERT into Flashcards(user, english, chinese, seen, correct) VALUES (133, @0, @1, 10, 2)';
+db.run(cmdStr_in, eng, chi, insertCallback);
+
+db.get('SELECT * FROM Flashcards WHERE seen>5', dataCallback);
+db.all('SELECT * FROM Flashcards WHERE user = 133', arrayCallback);
 
 // Always use the callback for database operations and print out any
 // error messages you get.
@@ -21,5 +29,29 @@ function tableCreationCallback(err) {
     } else {
 	console.log("Database created");
 	db.close();
+    }
+}
+
+function insertCallback(err) {
+    if(err) {
+        console.log(err);
+    }
+}
+
+function dataCallback(err, rowData) {
+    if (err) {
+        console.log("error: ", err);
+    }
+    else {
+        console.log("got: ", rowData, "\n");
+    }
+}
+
+function arrayCallback(err, arrayData) {
+    if(err) {
+        console.log("error: ", err,"\n");
+    }
+    else {
+        console.log("array: ", arrayData, "\n");
     }
 }
